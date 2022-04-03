767843a9bd9e22814bd16eaa127d3f913f0f0abc
Valentin271 <valentin.doreau@free.fr>
2022-04-03 21:57:03 +0200

    updated parenthesis to comply with compiler warnings

--- src/bot.c
+++ src/bot.c
@@ -4,7 +4,7 @@ Movement bot(Vector2 ballPos, Vector2 botPos)
 {
     // If ball is too much on the left
     // or ball is not aligned with player (20px margin)
-    if (ballPos.x < GetScreenWidth()/3 || (ballPos.y > botPos.y + 20) && (ballPos.y < botPos.y + playerSize.y - 20)) {
+    if (ballPos.x < GetScreenWidth()/3 || ((ballPos.y > botPos.y + 20) && (ballPos.y < botPos.y + playerSize.y - 20))) {
         return NONE;
     }
 
